a811ec4697870f476279591dde31cc514abcfa72
ManojKumarGaddameedi <92380229+ManojKumarGaddameedi@users.noreply.github.com>
2021-11-25 11:20:08 +0530

    deploy with git

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         hai iam with git injection commands
         </h1>
 
         <p className={styles.description}>
